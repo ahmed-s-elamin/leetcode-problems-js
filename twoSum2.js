109e5b8c0b219101cc5e07598050ceeec35bdbea
ahmed elamin <ahmedselamin@gmail.com>
2023-02-11 21:14:33 +0200

    done

--- twoSum2.js
+++ twoSum2.js
@@ -1,6 +1,6 @@
 var twoSum = function (numbers, target) {
-  let left = 0,
-    right = numbers.length - 1;
+  let left = 0;
+  let right = numbers.length - 1;
   while (left < right) {
     let sum = numbers[left] + numbers[right];
     if (sum === target) {
